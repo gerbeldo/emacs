0a34161bee1246df67e9dc4a8d1a2d433b772e5f
german <gerbeldo@gmail.com>
2018-09-07 20:35:40 -0300

    adds menu bar, sometimes it's useful

--- elisp/base.el
+++ elisp/base.el
@@ -72,7 +72,7 @@
 (global-auto-revert-mode t)
 
 ;; Disable toolbar & menubar
-(menu-bar-mode -1)
+;;(menu-bar-mode -1)
 (when (fboundp 'tool-bar-mode)
   (tool-bar-mode -1))
 (when (  fboundp 'scroll-bar-mode)
