3e19ce3f173aa3a09f6f288d7c1a7366877ee328
german <german@mac_mini>
2020-02-18 13:17:51 -0300

    .custom changed

--- .custom.el
+++ .custom.el
@@ -4,10 +4,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ess-indent-with-fancy-comments nil)
+ '(org-agenda-files
+   (quote
+    ("~/Dropbox/org/projects/2020-01-20_kar4_mutants.org" "~/Dropbox/org/projects/2020_misc.org" "~/Dropbox/org/projects/2019-11_post_SAIB.org")))
  '(org-directory "~/Dropbox/org/capture")
  '(package-selected-packages
    (quote
-    (yasnippet magit-popup company avy paradox which-key wgrep use-package undo-tree solarized-theme smex smartparens smart-mode-line rainbow-delimiters py-autopep8 pip-requirements org-bullets neotree multiple-cursors magit hlinum flycheck expand-region exec-path-from-shell ess elpy dashboard counsel ace-window))))
+    (yasnippet magit-popup company avy paradox which-key wgrep use-package undo-tree solarized-theme smex smartparens smart-mode-line rainbow-delimiters py-autopep8 pip-requirements org-bullets neotree multiple-cursors magit hlinum flycheck expand-region exec-path-from-shell ess elpy dashboard counsel ace-window)))
+ '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
