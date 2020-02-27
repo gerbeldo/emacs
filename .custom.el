f1cc7d3091c93595bba7a630bdfd3078dc64696d
german <german@MacBook.local>
2020-02-27 08:28:58 -0300

    added sum1 to agenda files

--- .custom.el
+++ .custom.el
@@ -6,11 +6,11 @@
  '(ess-indent-with-fancy-comments nil)
  '(org-agenda-files
    (quote
-    ("~/Dropbox/org/projects/2020-01-20_kar4_mutants.org" "~/Dropbox/org/projects/2020_misc.org" "~/Dropbox/org/projects/2019-11_post_SAIB.org")))
+    ("~/Dropbox/org/projects/2020-02_sum1.org" "~/Dropbox/org/projects/2020-01-20_kar4_mutants.org" "~/Dropbox/org/projects/2020_misc.org" "~/Dropbox/org/projects/2019-11_post_SAIB.org")))
  '(org-directory "~/Dropbox/org/capture")
  '(package-selected-packages
    (quote
-    (yasnippet magit-popup company avy paradox which-key wgrep use-package undo-tree solarized-theme smex smartparens smart-mode-line rainbow-delimiters py-autopep8 pip-requirements org-bullets neotree multiple-cursors magit hlinum flycheck expand-region exec-path-from-shell ess elpy dashboard counsel ace-window)))
+    (org-super-agenda yasnippet magit-popup company avy paradox which-key wgrep use-package undo-tree solarized-theme smex smartparens smart-mode-line rainbow-delimiters py-autopep8 pip-requirements org-bullets neotree multiple-cursors magit hlinum flycheck expand-region exec-path-from-shell ess elpy dashboard counsel ace-window)))
  '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
