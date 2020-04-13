91b08d71d910d79f74d0de4b73a504907bf9091d
german <german@MacBook.local>
2020-04-13 20:55:23 -0300

    custom.el

--- .custom.el
+++ .custom.el
@@ -5,16 +5,14 @@
  ;; If there is more than one, they won't work right.
  '(ess-indent-with-fancy-comments nil)
  '(org-agenda-files
-   (quote
-    ("~/Dropbox/org/projects/2020-02_ChIP_kar4_ste12.org" "~/Dropbox/org/projects/2020-02_sum1.org" "~/Dropbox/org/projects/2020-02_Y2H_ste12-kar4.org" "~/Dropbox/org/projects/2020-01-20_kar4_mutants.org" "~/Dropbox/org/projects/2020_misc.org" "~/Dropbox/org/projects/2019-11_post_SAIB.org")))
+   '("~/Dropbox/org/projects/2020-02_ChIP_kar4_ste12.org" "~/Dropbox/org/projects/2020-02_sum1.org" "~/Dropbox/org/projects/2020-02_Y2H_ste12-kar4.org" "~/Dropbox/org/projects/2020-01-20_kar4_mutants.org" "~/Dropbox/org/projects/2020_misc.org" "~/Dropbox/org/projects/2019-11_post_SAIB.org"))
  '(org-directory "~/Dropbox/org/capture")
  '(package-selected-packages
-   (quote
-    (dired-sidebar org-super-agenda yasnippet magit-popup company avy paradox which-key wgrep use-package undo-tree solarized-theme smex smartparens smart-mode-line rainbow-delimiters py-autopep8 pip-requirements org-bullets multiple-cursors magit hlinum flycheck expand-region exec-path-from-shell ess elpy dashboard counsel ace-window)))
+   '(vscode-icon dired-sidebar org-super-agenda yasnippet magit-popup company avy paradox which-key wgrep use-package undo-tree solarized-theme smex smartparens smart-mode-line rainbow-delimiters py-autopep8 pip-requirements org-bullets multiple-cursors magit flycheck expand-region exec-path-from-shell ess elpy dashboard counsel ace-window))
  '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(line-number ((t nil))))
